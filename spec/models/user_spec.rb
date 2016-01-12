b9922a7cb341c865ae46da52a8a7d14b15a4bc49
Viksit Arora <viksit@mit.edu>
2016-01-13 00:03:02 +0530

    gets email uniqueness to pass by making it case insensitive

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe User, type: :model do
     it { should respond_to(:password) }
     it { should respond_to(:password_confirmation) }
     it { should validate_presence_of(:email) }
-    it { should validate_uniqueness_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
     it { should validate_confirmation_of(:password) }
     it { should allow_value('example@domain.com').for(:email) }
   end
