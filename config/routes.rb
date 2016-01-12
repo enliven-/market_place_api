0556e39de7e4da2e99365b9e39583d7e68a77780
Viksit Arora <viksit@mit.edu>
2016-01-12 23:11:39 +0530

    sets up rspec; adds versioning constraint on routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
 
-  end
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
 
+    end
+
+  end
+  
 end
