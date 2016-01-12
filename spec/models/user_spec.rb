e817bcad92391b57d08348784c7cc198c6a92353
Viksit Arora <viksit@mit.edu>
2016-01-12 23:49:10 +0530

    fixes shoulda bug by setting up config in rails_helper

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,12 +5,16 @@ RSpec.describe User, type: :model do
   describe User do
     before  { @user = FactoryGirl.build(:user) }
     subject { @user }
+    
+    it { should be_valid }
 
     it { should respond_to(:email) }
     it { should respond_to(:password) }
     it { should respond_to(:password_confirmation) }
-
-    it { should be_valid }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_confirmation_of(:password) }
+    it { should allow_value('example@domain.com').for(:email) }
   end
 
 end
