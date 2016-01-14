69ffd72eed19f7207bdfb5b1186cfe536747d085
Viksit Arora <viksit@mit.edu>
2016-01-14 12:28:23 +0530

    finishes modeling the product model along with user associations; adds requisite specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :auth_token, uniqueness: true
+  validates     :auth_token, uniqueness: true
   before_create :generate_authentication_token!
+
+  has_many :products, dependent: :destroy
 
   def generate_authentication_token!
     begin
